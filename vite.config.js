be79e37948499167a79d06ac9e4b313fccc3c881
zack10 <zacksmetz@yahoo.com>
2024-07-22 14:18:14 +0200

    fixing vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"],
+  resolve: {
+    alias: {
+      react: require.resolve("react"),
+      "react-dom": require.resolve("react-dom"),
+      "react-router-dom": require.resolve("react-router-dom"),
     },
   },
 });
